ed1dd3c7ded164298237a963406c106a969cc31c
sojib400 <sojibdas400@gmail.com>
2021-03-11 17:45:30 +0600

    corona 19

---
+++ src/components/CountryPicker/CountryPicker.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import { NativeSelect, FormControl } from "@material-ui/core";
+
+import { fetchCountries } from "../../api";
+
+import styles from "./CountryPicker.css";
+
+const Countries = ({ handleCountryChange }) => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setCountries(await fetchCountries());
+    };
+
+    fetchAPI();
+  }, []);
+
+  return (
+    <div style={{ width: "30%", margin: "auto", padding:"30px" }}>
+      <FormControl>
+        <NativeSelect
+          defaultValue=""
+          onChange={(e) => handleCountryChange(e.target.value)}
+        >
+          <option value="">Global</option>
+          {countries.map((country, i) => (
+            <option key={i} value={country}>
+              {country}
+            </option>
+          ))}
+        </NativeSelect>
+      </FormControl>
+    </div>
+  );
+};
+
+export default Countries;
